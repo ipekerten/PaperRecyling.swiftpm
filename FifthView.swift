2023b4d0d40f43ad684cb7dac06cc910b1c427c1
ipekerten <ipek.erten@ozu.edu.tr>
2025-02-11 10:38:47 +0100

    Added FourtView with the core motion. Created FifthView.

---
+++ FifthView.swift
@@ -0,0 +1,19 @@
+//
+//  FifthView.swift
+//  PaperRecyling
+//
+//  Created by Ipek Erten on 10/02/25.
+//
+
+import SwiftUI
+struct FifthView: View {
+    var body: some View {
+            Text("Forza Napoli!")
+            
+            .navigationBarHidden(true)
+    }
+}
+
+#Preview {
+    FifthView()
+}
--- FourthView.swift
+++ FourthView.swift
@@ -1,18 +1,68 @@
-//
-//  FourthView.swift
-//  PaperRecyling
-//
-//  Created by Ipek Erten on 10/02/25.
-//
-
 import SwiftUI
+import CoreMotion
+
 struct FourthView: View {
+    @State private var backgroundColor: Color = .white
+    @State private var colorIndex: Int = 0
+    @State private var showNextButton: Bool = false
+    
+    private let motionManager = CMMotionManager()
+    private let accelerationThreshold = 2.5
+    private let colors: [Color] = [.white, .blue, .cyan, .mint, .pink, .yellow, .purple, .green, .orange, .red]
+
     var body: some View {
-        Text("Oi")
-            .font(.largeTitle)
-            .padding()
+        ZStack {
+            Rectangle()
+                .fill(backgroundColor)
+                .edgesIgnoringSafeArea(.all)
+                .onAppear {
+                    startMonitoringAccelerometer()
+                }
+            
+            if showNextButton {
+                NavigationLink("Next") {
+                    FifthView()
+                }
+                .padding()
+                .background(Color.black.opacity(0.7))
+                .foregroundColor(.white)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .position(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height - 100)
+            }
+        }
         
-            .navigationBarHidden(true)
+        .navigationBarHidden(true)
+    }
+
+    private func startMonitoringAccelerometer() {
+        guard motionManager.isAccelerometerAvailable else {
+            print("Accelerometer not available")
+            return
+        }
+        
+        motionManager.accelerometerUpdateInterval = 0.1
+        motionManager.startAccelerometerUpdates(to: .main) { data, error in
+            guard let data = data, !self.showNextButton else { return }
+            
+            let acceleration = data.acceleration
+            let magnitude = sqrt(acceleration.x * acceleration.x +
+                                 acceleration.y * acceleration.y +
+                                 acceleration.z * acceleration.z)
+            
+            if magnitude > self.accelerationThreshold {
+                DispatchQueue.main.async {
+                    if self.colorIndex < self.colors.count - 1 {
+                        self.colorIndex += 1
+                        self.backgroundColor = self.colors[self.colorIndex]
+                    }
+                    
+                    if self.colorIndex == self.colors.count - 1 {
+                        self.showNextButton = true
+                        self.motionManager.stopAccelerometerUpdates()
+                    }
+                }
+            }
+        }
     }
 }
 
